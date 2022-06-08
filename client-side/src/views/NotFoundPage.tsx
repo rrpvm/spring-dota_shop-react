13bd6cec2aa27a5a84c9fbaaa8dcc0fd945d5815
rrpvm <funikovsasa@gmail.com>
2022-06-08 14:09:52 +0700

    404 page animation update)

--- client-side/src/views/NotFoundPage.tsx
+++ client-side/src/views/NotFoundPage.tsx
@@ -1,19 +1,30 @@
 import { useEffect } from 'react';
+import { useState } from 'react';
 import { useRef } from 'react';
 import '../styles/404.css'
 const NotFoundPage: React.FC = () => {
-    const width = 1280;
-    const height = 720;
+    const width = window.screen.width;
+    const height = window.screen.height;
+    const precision = 150;
     const canvas = useRef<HTMLCanvasElement | null>(null);
-    let context: CanvasRenderingContext2D | null = null;
+    const [context, setContext] = useState<CanvasRenderingContext2D | null>(null);
     let particles: { x: number, y: number }[] = [];
     let time: number = 0;
     let accumulator: number = 0;
+    let mouseX = 0;
+    let mouseY = 0;
+    let sinTable: number[] = [];
+    let cosTable: number[] = [];
     useEffect(() => {
         if (canvas === null) return;
         if (canvas.current === null) return;
-        context = canvas.current?.getContext('2d');
+        setContext(canvas.current.getContext('2d'));
         if (context === null) return;
+        for (let i = 0; i < Math.PI * 2.0; i += (2.0 * Math.PI) / precision) {
+            sinTable.push(Math.sin(i));
+            cosTable.push(Math.cos(i));
+        }
+        console.log(sinTable.length);
         initialize(context);
     }, []);
     const initialize = (ctx: CanvasRenderingContext2D) => {
@@ -26,8 +37,8 @@ const NotFoundPage: React.FC = () => {
         ctx.fillText('404', width / 2, height / 2 + fontSize / 4);
         const data = ctx.getImageData(0, 0, width, height).data;
         const data32 = new Uint32Array(data.buffer);
-        for (let x = 0; x < width; x += 5) {
-            for (let y = 0; y < height; y += 5) {
+        for (let x = 0; x < width; x += 15) {
+            for (let y = 0; y < height; y += 15) {
                 const _color = data32[y * width + x];
                 if (_color !== 0xFF000000) particles.push({ x, y });
             }
@@ -39,22 +50,25 @@ const NotFoundPage: React.FC = () => {
         let old = time;
         time = performance.now();
         const delta = time - old;
-        accumulator += delta/100;
+        accumulator += delta / 100;
         context.clearRect(0, 0, width, height);
         context.fillStyle = "#FF000000";
         context.fillRect(0, 0, width, height);
-
-       
+        const color = `hsl(${Math.sin(accumulator / 200) * 360},100%,50%)`;
+        context.fillStyle = color;
+        //  let dX = 3 * cosTable[Math.floor(i * 14 + accumulator * 20) % precision]; //Math.cos(i*14+accum)
+        //let dY = 3 * sinTable[Math.floor(((i * 20 + accumulator * 20) % precision))];//          Math.sin(i * 20 + accumulator);
         for (let i = 0; i < particles.length; i++) {
-            const color = `hsl(${Math.sin(accumulator/1000+i)*360},100%,50%)`;
-            context.fillStyle = color;
-            const dX = (4 / 10) * Math.cos(i * 14 + accumulator);
-            const dY = (4 / 10) * Math.sin(i * 20 + accumulator);
-            const radius = 4 * Math.abs(Math.sin(i + accumulator/3));
+            const length = Math.sqrt(Math.pow(mouseX - particles[i].x, 2) + Math.pow(mouseY - particles[i].y, 2));
+            let dX = (mouseX - particles[i].x) * 15.0 / length + 3 * cosTable[Math.floor(i * 14 + accumulator * 20) % precision];
+            let dY = (mouseY - particles[i].y) * 15.0 / length + 3 * sinTable[Math.floor(((i * 20 + accumulator * 20) % precision))];
+            const finalX = particles[i].x + dX;
+            const finalY = particles[i].y + dY;
+            const radius = 7 * (Math.abs(sinTable[Math.floor((i * 200 + accumulator * 5) % precision)]));
             context.beginPath();
             context.arc(
-                particles[i].x + dX,
-                particles[i].y + dY,
+                finalX,
+                finalY,
                 radius,
                 0, 2 * Math.PI,
                 false);
@@ -62,8 +76,12 @@ const NotFoundPage: React.FC = () => {
         }
         requestAnimationFrame(render);
     }
+    const onMouseMove = (e: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
+        mouseX = e.clientX;
+        mouseY = e.clientY;
+    }
     return (
-        <canvas className="canvas404" ref={canvas} width={width} height={height}>
+        <canvas className="canvas404" ref={canvas} width={width} height={height} onMouseMove={onMouseMove}>
 
         </canvas>
     );
