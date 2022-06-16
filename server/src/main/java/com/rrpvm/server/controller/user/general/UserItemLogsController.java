5446f950b396222bbb6ac4442876524e2c1ffc41
rrpvm <funikovsasa@gmail.com>
2022-06-17 01:41:27 +0700

    backend : pre-release

--- server/src/main/java/com/rrpvm/server/controller/user/general/UserItemLogsController.java
+++ server/src/main/java/com/rrpvm/server/controller/user/general/UserItemLogsController.java
@@ -9,8 +9,11 @@ import com.rrpvm.server.model.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
